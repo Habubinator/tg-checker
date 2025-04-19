ee7c63f55afb750bfd04c412f460e964bae0337d
Habubinator <61784574+Habubinator@users.noreply.github.com>
2025-04-19 16:02:29 +0300

    yahoo

--- src/bot/index.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-import TelegramBot, { CallbackQuery } from "node-telegram-bot-api";
-
-const token = process.env.BOT_TOKEN;
-process.env["NTBA_FIX_350"] = "1";
-process.env["NTBA_FIX_319"] = "1";
-
-export const bot = new TelegramBot(token, { polling: true });
